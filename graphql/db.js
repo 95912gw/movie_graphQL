f77913e84653b4fe8a468094a91a4b0965562a8f
95912gw <gw777seong@gmail.com>
2021-02-23 18:14:37 +0900

    Add db

---
+++ graphql/db.js
@@ -0,0 +1,31 @@
+export const people = [
+    {
+        id: 1,
+        name: "Gyuwon",
+        age: 27,
+        gender: "male"
+    },
+    {
+        id: 2,
+        name: "Json",
+        age: 32,
+        gender: "female"
+    },
+    {
+        id: 3,
+        name: "John",
+        age: 22,
+        gender: "female"
+    },
+    {
+        id: 4,
+        name: "Sarah",
+        age: 35,
+        gender: "male"
+    },
+    {
+        id: 5,
+        name: "Jean",
+        age: 18,
+        gender: "female"
+    }]
